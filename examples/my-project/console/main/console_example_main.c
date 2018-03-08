e668a2141b271245dbb869c20975b778b22c3c95
XuanZe <119524428@qq.com>
2018-03-08 18:08:15 +0800

    修改例程

--- examples/my-project/console/main/console_example_main.c
+++ examples/my-project/console/main/console_example_main.c
@@ -21,7 +21,7 @@
 #include "nvs.h"
 #include "nvs_flash.h"
 
-static const char* TAG = "example";
+static const char *TAG = "example";
 
 /* Console command history can be stored to and loaded from a file.
  * The easiest way to do this is to use FATFS filesystem on top of
@@ -36,30 +36,34 @@ static void initialize_filesystem()
 {
     static wl_handle_t wl_handle;
     const esp_vfs_fat_mount_config_t mount_config = {
-            .max_files = 4,
-            .format_if_mount_failed = true
-    };
-    esp_err_t err = esp_vfs_fat_spiflash_mount(MOUNT_PATH, "storage", &mount_config, &wl_handle);
-    if (err != ESP_OK) {
+        .max_files = 4,
+        .format_if_mount_failed = true};
+    esp_err_t err =esp_vfs_fat_spiflash_mount(  MOUNT_PATH,
+                                                "storage",
+                                                &mount_config,
+                                                &wl_handle);
+    if (err != ESP_OK)
+    {
         ESP_LOGE(TAG, "Failed to mount FATFS (0x%x)", err);
         return;
     }
 }
 #endif // CONFIG_STORE_HISTORY
-
+//加载nvs
 static void initialize_nvs()
 {
     esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
-        ESP_ERROR_CHECK( nvs_flash_erase() );
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
         err = nvs_flash_init();
     }
     ESP_ERROR_CHECK(err);
 }
-
+//加载console
 static void initialize_console()
 {
-    /* Disable buffering on stdin and stdout */
+    /* 禁用 stdin,stdout缓冲 */
     setvbuf(stdin, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
 
@@ -69,31 +73,30 @@ static void initialize_console()
     esp_vfs_dev_uart_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
 
     /* Install UART driver for interrupt-driven reads and writes */
-    ESP_ERROR_CHECK( uart_driver_install(CONFIG_CONSOLE_UART_NUM,
-            256, 0, 0, NULL, 0) );
+    ESP_ERROR_CHECK(uart_driver_install(CONFIG_CONSOLE_UART_NUM,
+                                        256, 0, 0, NULL, 0));
 
     /* Tell VFS to use UART driver */
     esp_vfs_dev_uart_use_driver(CONFIG_CONSOLE_UART_NUM);
 
     /* Initialize the console */
     esp_console_config_t console_config = {
-            .max_cmdline_args = 8,
-            .max_cmdline_length = 256,
+        .max_cmdline_args = 8,
+        .max_cmdline_length = 256,
 #if CONFIG_LOG_COLORS
-            .hint_color = atoi(LOG_COLOR_CYAN)
+        .hint_color = atoi(LOG_COLOR_CYAN)
 #endif
     };
-    ESP_ERROR_CHECK( esp_console_init(&console_config) );
+    ESP_ERROR_CHECK(esp_console_init(&console_config));
 
     /* Configure linenoise line completion library */
-    /* Enable multiline editing. If not set, long commands will scroll within
-     * single line.
+    /* 启用多行编辑。 如果未设置，长命令将在单行内滚动。
      */
     linenoiseSetMultiLine(1);
 
-    /* Tell linenoise where to get command completions and hints */
+    /* 告诉linenoise从哪里获得命令完成和提示 */
     linenoiseSetCompletionCallback(&esp_console_get_completion);
-    linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
+    linenoiseSetHintsCallback((linenoiseHintsCallback *)&esp_console_get_hint);
 
     /* Set command history size */
     linenoiseHistorySetMaxLen(100);
@@ -107,22 +110,19 @@ static void initialize_console()
 void app_main()
 {
     initialize_nvs();
-
 #if CONFIG_STORE_HISTORY
     initialize_filesystem();
 #endif
-
     initialize_console();
 
-    /* Register commands */
+    /* TODO:注册命令 */
     esp_console_register_help_command();
     register_system();
-    register_wifi();
+    // register_wifi();
 
-    /* Prompt to be printed before each line.
-     * This can be customized, made dynamic, etc.
+    /* 提示在每行之前打印。 这可以定制，动态等。
      */
-    const char* prompt = LOG_COLOR_I "esp32> " LOG_RESET_COLOR;
+    const char *prompt = LOG_COLOR_I "esp32> " LOG_RESET_COLOR;
 
     printf("\n"
            "This is an example of ESP-IDF console component.\n"
@@ -130,48 +130,56 @@ void app_main()
            "Use UP/DOWN arrows to navigate through command history.\n"
            "Press TAB when typing command name to auto-complete.\n");
 
-    /* Figure out if the terminal supports escape sequences */
+    /* 找出终端是否支持转义序列 */
     int probe_status = linenoiseProbe();
-    if (probe_status) { /* zero indicates success */
+    if (probe_status)
+    { /* 零表示成功 */
         printf("\n"
                "Your terminal application does not support escape sequences.\n"
                "Line editing and history features are disabled.\n"
                "On Windows, try using Putty instead.\n");
         linenoiseSetDumbMode(1);
 #if CONFIG_LOG_COLORS
-        /* Since the terminal doesn't support escape sequences,
-         * don't use color codes in the prompt.
+        /* 由于终端不支持转义序列，因此不要在提示中使用颜色代码。
          */
         prompt = "esp32> ";
 #endif //CONFIG_LOG_COLORS
     }
 
-    /* Main loop */
-    while(true) {
-        /* Get a line using linenoise.
-         * The line is returned when ENTER is pressed.
+    /* 主循环 */
+    while (true)
+    {
+        /* 使用linenoise获得一条线。 当按下ENTER时，该行将被返回。
          */
-        char* line = linenoise(prompt);
-        if (line == NULL) { /* Ignore empty lines */
+        char *line = linenoise(prompt);
+        if (line == NULL)
+        { /* Ignore empty lines */
             continue;
         }
         /* Add the command to the history */
         linenoiseHistoryAdd(line);
 #if CONFIG_STORE_HISTORY
-        /* Save command history to filesystem */
+        /* 将命令历史保存到文件系统 */
         linenoiseHistorySave(HISTORY_PATH);
 #endif
 
-        /* Try to run the command */
+        /* 尝试运行该命令 */
         int ret;
         esp_err_t err = esp_console_run(line, &ret);
-        if (err == ESP_ERR_NOT_FOUND) {
+        if (err == ESP_ERR_NOT_FOUND)
+        {
             printf("Unrecognized command\n");
-        } else if (err == ESP_ERR_INVALID_ARG) {
+        }
+        else if (err == ESP_ERR_INVALID_ARG)
+        {
             // command was empty
-        } else if (err == ESP_OK && ret != ESP_OK) {
+        }
+        else if (err == ESP_OK && ret != ESP_OK)
+        {
             printf("Command returned non-zero error code: 0x%x\n", ret);
-        } else if (err != ESP_OK) {
+        }
+        else if (err != ESP_OK)
+        {
             printf("Internal error: 0x%x\n", err);
         }
         /* linenoise allocates line buffer on the heap, so need to free it */
