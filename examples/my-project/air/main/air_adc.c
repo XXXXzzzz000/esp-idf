5a0a5b7088c20e11253f9eeed53a9e29524ddbf2
XuanZe <119524428@qq.com>
2018-03-14 15:58:02 +0800

    修改adc任务:改为固定30s执行一次,添加adc任务挂起和恢复命令

--- examples/my-project/air/main/air_adc.c
+++ examples/my-project/air/main/air_adc.c
@@ -15,12 +15,13 @@ static const char *TAG = "AIR_ADC";
 #define DEFAULT_VREF 1100 //使用adc2_vref_to_gpio（）来获得更好的估计
 #define NO_OF_SAMPLES 64  //多重采样
 
+xTaskHandle xAirAdcHandle = NULL;
 static esp_adc_cal_characteristics_t *adc_chars;
 static const adc_channel_t channel = ADC_CHANNEL_6;  //GPIO34 if ADC1, GPIO14 if ADC2
 static const adc_channel_t channel2 = ADC_CHANNEL_5; //GPIO32 具体查看ESP32技术参考手册
 static const adc_atten_t atten = ADC_ATTEN_DB_11;
 static const adc_unit_t unit = ADC_UNIT_1;
-
+static uint64_t adc_runtime = 0;
 #if 0
 //检查adc的参考电压值
 static void check_efuse()
@@ -45,7 +46,7 @@ static void check_efuse()
         ESP_LOGI(TAG, "eFuse Vref: NOT supported\n");
     }
 }
-#endif
+
 //打印adc检查值
 static void print_char_val_type(esp_adc_cal_value_t val_type)
 {
@@ -62,6 +63,7 @@ static void print_char_val_type(esp_adc_cal_value_t val_type)
         ESP_LOGI(TAG, "Characterized using Default Vref\n");
     }
 }
+#endif
 
 //初始化adc
 static void air_adc_init()
@@ -70,23 +72,18 @@ static void air_adc_init()
     //检查两点或Vref是否被烧入eFuse
     // check_efuse();
 
-    //配置ADC
-    if (unit == ADC_UNIT_1)
-    {
-        adc1_config_width(ADC_WIDTH_BIT_12);
-        adc1_config_channel_atten(channel, atten);
-        adc1_config_channel_atten(channel2, atten);
-    }
-    else
-    {
-        adc2_config_channel_atten((adc2_channel_t)channel, atten);
-    }
+//配置ADC
+    //位宽
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    //通道衰减
+    adc1_config_channel_atten(channel, atten);
+    adc1_config_channel_atten(channel2, atten);
 
-    // ADC特性描述
+// ADC特性描述
     adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
     //adc1 衰减ADC_ATTEN_DB_11 adc位宽12 参考电压DEFAULT_VREF adc特性指针
     esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
-    print_char_val_type(val_type);
+    // print_char_val_type(val_type);
 }
 //获取自系统运行以来的秒数
 uint64_t get_time_s()
@@ -99,7 +96,7 @@ static uint32_t air_adc_get_voltage()
 {
     uint32_t adc_reading = 0;
     uint32_t adc_reading2 = 0;
-    //多重采样两个通道,求其均值
+//多重采样两个通道,求其均值
     for (int i = 0; i < NO_OF_SAMPLES; i++)
     {
         adc_reading += adc1_get_raw((adc1_channel_t)channel);
@@ -107,25 +104,88 @@ static uint32_t air_adc_get_voltage()
     }
     adc_reading /= NO_OF_SAMPLES;
     adc_reading2 /= NO_OF_SAMPLES;
-    //转换 adc_reading to voltage in mV
+//转换 adc_reading to voltage in mV
     uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
     uint32_t voltage2 = esp_adc_cal_raw_to_voltage(adc_reading2, adc_chars);
     ESP_LOGI(TAG, "Raw: %d\tVoltage: %dmV\tTime:%lld\n", adc_reading, voltage, get_time_s());
     ESP_LOGI(TAG, "Raw2: %d\tVoltage2: %dmV\tTime:%lld\n", adc_reading2, voltage2, get_time_s());
 
-    //将读取的示数写入到文件内
+//将读取的示数写入到文件内
     cmd_storge_write(1, adc_reading, voltage, get_time_s());
     cmd_storge_write(2, adc_reading2, voltage2, get_time_s());
     return voltage;
 }
 
-//获取adc值的任务
+//获取adc值的任务 30s运行一次
 void air_adc_get_task(void *parm)
 {
+    static portTickType xLastWakeTime;
+    const portTickType xFrequency = 3000;
+
+    // 使用当前时间初始化变量xLastWakeTime
+    xLastWakeTime = xTaskGetTickCount();
     air_adc_init();
     while (1)
     {
+        //等待下一个周期
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+        // 需要周期性执行代码放在这里
         air_adc_get_voltage();
-        vTaskDelay(300);
+        //adc运行时间增加
+        adc_runtime++;
+    }
+}
+
+/* console 命令 */
+static void cmd_adc_suspend(int argc, char **argv)
+{
+    if (xAirAdcHandle == NULL)
+    {
+        ESP_LOGE(TAG, "air task handler is null");
+        return;
+    }
+    vTaskSuspend(xAirAdcHandle);
+}
+static void cmd_adc_resume(int argc, char **argv)
+{
+    if (xAirAdcHandle == NULL)
+    {
+        ESP_LOGE(TAG, "air task handler is null");
+        return;
+    }
+    xTaskToResume(xAirAdcHandle);
+}
+
+/** commonds register*/
+void register_adc()
+{
+    esp_console_cmd_t cmd_table[] =
+        {
+
+            {
+                .command = "adc_resume",
+                .help = "",
+                .hint = NULL,
+                .func = &cmd_adc_resume,
+            },
+            {
+                .command = "adc_suspend",
+                .help = "",
+                .hint = NULL,
+                .func = &cmd_adc_suspend,
+            },
+            {
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+            }
+
+        };
+
+    for (esp_console_cmd_t *p = cmd_table; p->command != NULL; p++)
+    {
+        ESP_ERROR_CHECK(esp_console_cmd_register(p));
     }
 }
--- examples/my-project/air/main/air_adc.h
+++ examples/my-project/air/main/air_adc.h
@@ -1,3 +1,15 @@
 #pragma once
+
+
+typedef struct
+{
+    MQ_135 = 1, MQ_136 = 2
+} air_sensor_t;
+// typedef struct
+// {
+//     CALIBRATION,COLLECT
+// }air_sensor_state_t;
+extern xTaskHandle xAirAdcHandle=NULL;
 void air_adc_get_task(void *parm);
+void register_adc();
 
