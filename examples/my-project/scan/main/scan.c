d8887dc3ce0b30cb91097db952b650de493c47e8
XuanZe <119524428@qq.com>
2018-03-15 18:12:23 +0800

    修改注释

--- examples/my-project/scan/main/scan.c
+++ examples/my-project/scan/main/scan.c
@@ -20,6 +20,19 @@
     are scanned, and connection will start with the best network. The networks
     can be sorted based on Authentication Mode or Signal Strength. The priority
     for the Authentication mode is:  WPA2 > WPA > WEP > Open
+
+    此示例显示如何使用全部通道扫描或快速扫描进行连接
+     到Wi-Fi网络。
+
+     在快速扫描模式下，只要第一个网络匹配，扫描就会停止
+     找到SSID。 在这种模式下，应用程序可以设置阈值
+     认证模式和信号强度。 网络不符合
+     阈值要求将被忽略。
+
+     在全频道扫描模式下，扫描将仅在所有频道之后结束
+     被扫描，并且连接将从最佳网络开始。 网络
+     可以根据认证模式或信号强度进行排序。 优先级
+     对于身份验证模式是：WPA2> WPA> WEP>打开
 */
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
@@ -28,7 +41,7 @@
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
 
-/*Set the SSID and Password via "make menuconfig"*/
+/*通过"make menuconfig"配置 SSID 和 Password via */
 #define DEFAULT_SSID CONFIG_WIFI_SSID
 #define DEFAULT_PWD CONFIG_WIFI_PASSWORD
 
@@ -90,14 +103,16 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     return ESP_OK;
 }
 
-/* Initialize Wi-Fi as sta and set scan method */
+/* 初始化 Wi-Fi 为 sta模式 并设置扫描方式 */
 static void wifi_scan(void)
 {
+//初始化tcp/ip适配器
     tcpip_adapter_init();
     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
-
+//初始化wifi-config
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+//wifi配置
     wifi_config_t wifi_config = {
         .sta = {
             .ssid = DEFAULT_SSID,
@@ -108,6 +123,7 @@ static void wifi_scan(void)
             .threshold.authmode = DEFAULT_AUTHMODE,
         },
     };
+//设置wifi模式,配置,开启wifi
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
@@ -115,7 +131,7 @@ static void wifi_scan(void)
 
 void app_main()
 {
-    // Initialize NVS
+    // 初始化 NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
         ESP_ERROR_CHECK(nvs_flash_erase());
