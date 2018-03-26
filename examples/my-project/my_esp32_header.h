8342401c1bda1b0dd4ce8f8b1cfcda26edc60bc5
XuanZe <119524428@qq.com>
2018-03-26 12:10:43 +0800

    项目配置文件修改

--- examples/my-project/my_esp32_header.h
+++ examples/my-project/my_esp32_header.h
@@ -30,6 +30,25 @@
 #include "sdkconfig.h"
 
 
-#include "air_adc.h"
-#include "air_storge.h"
-#include "cmd_decl.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event_loop.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
