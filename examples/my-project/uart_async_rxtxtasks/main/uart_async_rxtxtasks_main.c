c5b75b829780221d5c9d1c508fc1872fd4b619ce
XuanZe <119524428@qq.com>
2018-04-02 21:21:04 +0800

    添加tf

--- examples/my-project/uart_async_rxtxtasks/main/uart_async_rxtxtasks_main.c
+++ examples/my-project/uart_async_rxtxtasks/main/uart_async_rxtxtasks_main.c
@@ -14,12 +14,85 @@
 #include "soc/uart_struct.h"
 #include "string.h"
 
+#include "TinyFrame.h"
+
 static const int RX_BUF_SIZE = 256;
 
 #define TXD_PIN (GPIO_NUM_4)
 #define RXD_PIN (GPIO_NUM_5)
+#define TF_TAG "TintFrame"
 
-void init() {
+static const char *TAG = "serial";
+TinyFrame my_tf;
+TinyFrame *demo_tf = &my_tf;
+typedef unsigned char *pu8;
+bool do_corrupt = false;
+
+
+// helper func for testing
+void dumpFrame(const uint8_t *buff, size_t len)
+{
+    size_t i;
+    ESP_LOGI("tf_dump", "dumpFrame:\r\n");
+    for (i = 0; i < len; i++) {
+        ESP_LOGI("tf_dump", "%3u %02X", buff[i], buff[i]);
+        if (buff[i] >= 0x20 && buff[i] < 127) {
+            ESP_LOGI("tf_dump", " %c", buff[i]);
+        } else {
+            ESP_LOGI("tf_dump", " .");
+        }
+        ESP_LOGI("tf_dump", "\r\n");
+    }
+    ESP_LOGI("tf_dump", "--- end of frame ---\r\n\r\n");
+}
+
+void dumpFrameInfo(TF_Msg *msg)
+{
+    ESP_LOGI("tf_dump", "dumpFrameInfo:\r\n");
+    ESP_LOGI("tf_dump", "Frame info\r\n"
+             "  type: %02Xh\r\n"
+             "  data: \"%.*s\"\r\n"
+             "   len: %u\r\n"
+             "    id: %Xh\r\n\r\n",
+             msg->type, msg->len, msg->data, msg->len, msg->frame_id);
+}
+
+void TF_WriteImpl(TinyFrame *tf, const uint8_t *buff, uint32_t len)
+{
+    // ESP_LOGI(TF_TAG, "--------------------\r\n");
+    // ESP_LOGI(TF_TAG, "\033[32mTF_WriteImpl - sending frame:\033[0m\r\n");
+
+    ESP_LOGI("TF_WriteImpl", "len=%d", len);
+    // 打印发送的数据
+    ESP_LOG_BUFFER_CHAR("TF_WriteImpl:", buff, len);
+    uint8_t *xbuff = (uint8_t *)buff;
+    //校验和
+    if (do_corrupt) {
+        ESP_LOGI("TF_WriteImpl", "(corrupting to test checksum checking...)\r\n");
+        xbuff[8]++;
+    }
+    uart_write_bytes(UART_NUM_1, (const char * )buff, len);
+    // dumpFrame(xbuff, len);
+    // 将它发回，就好像我们收到了它
+    // TF_Accept(tf, xbuff, len);
+}
+
+/** 一个监听器函数的示例 */
+TF_Result myListener(TinyFrame *tf, TF_Msg *msg)
+{
+    dumpFrameInfo(msg);
+    return TF_STAY;
+}
+
+TF_Result testIdListener(TinyFrame *tf, TF_Msg *msg)
+{
+    ESP_LOGI(TF_TAG, "OK - ID Listener triggered for msg!\r\n");
+    dumpFrameInfo(msg);
+    return TF_CLOSE;
+}
+
+void init()
+{
     const uart_config_t uart_config = {
         .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
@@ -33,10 +106,9 @@ void init() {
     uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
 }
 
-int sendData(const char* logName, const char* data)
+int sendData(const char *logName, const char *data)
 {
-    const int len = strlen(data);
-    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, 256);
     ESP_LOGI(logName, "Wrote %d bytes", txBytes);
     return txBytes;
 }
@@ -46,8 +118,12 @@ static void tx_task()
     static const char *TX_TASK_TAG = "TX_TASK";
     esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
     while (1) {
-        sendData(TX_TASK_TAG, "Hello world\n");
+        char buf[256] = "{m:0}";
+        int txBytes = uart_write_bytes(UART_NUM_1, buf, 256);
+        // ESP_LOGI(logName, "Wrote %d bytes", txBytes);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
+        strcpy(buf, "{m:1}");
+        txBytes = uart_write_bytes(UART_NUM_1, buf, 256);
     }
 }
 
@@ -55,14 +131,16 @@ static void rx_task()
 {
     static const char *RX_TASK_TAG = "RX_TASK";
     esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
-    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
+    uint8_t *data = (uint8_t *) malloc(RX_BUF_SIZE + 1);
     while (1) {
-        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_RATE_MS);
+        int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 10 / portTICK_RATE_MS);
         if (rxBytes > 0) {
-            data[rxBytes] = 0;
+            // data[rxBytes] = 0;
+            TF_Accept(demo_tf, &data, rxBytes);
             ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
             ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
         }
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
     free(data);
 }
@@ -70,6 +148,56 @@ static void rx_task()
 void app_main()
 {
     init();
-    xTaskCreate(rx_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
-    xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES, NULL);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    TF_Msg msg;
+    const char *longstr = "Lorem ipsum dolor sit amet.";
+
+    //设置TinyFrame库
+    TF_InitStatic(&my_tf, TF_MASTER);
+    // 添加lintener
+    TF_AddGenericListener(demo_tf, myListener);
+
+    ESP_LOGI(TF_TAG, "------ Simulate sending a message --------\r\n");
+
+    TF_ClearMsg(&msg);
+
+    msg.type = 0x22;
+    msg.data = (pu8) "Hello TinyFrame";
+    msg.len = 16;
+    TF_Send(demo_tf, &msg);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+    msg.type = 0x33;
+    msg.data = (pu8) longstr;
+    msg.len = (TF_LEN) (strlen(longstr) + 1); // 添加null类型
+    TF_Send(demo_tf, &msg);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+    msg.type = 0x44;
+    msg.data = (pu8) "Hello2";
+    msg.len = 7;
+    TF_Send(demo_tf, &msg);
+
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    msg.len = 0;
+    msg.type = 0x77;
+    TF_Query(demo_tf, &msg, testIdListener, 0);
+
+    ESP_LOGI(TF_TAG, "This should fail:\r\n");
+
+    // 测试校验和
+    do_corrupt = true;
+    msg.type = 0x44;
+    msg.data = (pu8) "Hello2";
+    msg.len = 7;
+    TF_Send(demo_tf, &msg);
+
+    // xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES, NULL);
+    // vTaskDelay(2000 / portTICK_PERIOD_MS);
+    while (1) {
+        ESP_LOGE("info", "running");
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+    // xTaskCreate(tx_task, "uart_tx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 1, NULL);
 }
